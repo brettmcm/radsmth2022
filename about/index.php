138deee8cc7eb3a644a71763f1dd1b03d8e807b4
Brett McMillin <brettmcm@bretts-mbp.lan>
2020-02-29 11:16:56 -0800

    Updated content in all sections

--- about/index.php
+++ about/index.php
@@ -12,18 +12,22 @@
     <?php include '../components/header.php';?>
 
     <div>
-        <h2>&mdash;<br>About</h2>
 
         <div class="layout">
             <img src="../lib/img/brett.jpg" class="portrait">
             <div class="copy">
-                <p>I've been working as a graphic designer for over a decade and something about branding small businesses and startups.</p>
+                <h2>&mdash;<br>Hello</h2>
+                <p>I've been working as a graphic designer for over a decade. Experience working on everything from designing and developing web apps to directing video shoots has given me a generalist toolbox that's proven invaluable in my approach to branding and creative direction.</p>
                 <p>In 2016, I started Radsmth as an anti-agency model with the intention to deliver agency-quality design, a deliberate focus on customer service and transparent, affordable pricing.</p>
+
+                <h3>&mdash;<br>Art + Life</h3>
+                <p>Take a peak at the highlight reel of my personal life and art on <a href="http://www.instagram.com/vopka" target="_new">Instagram</a>.</p>
+
+                <h3>&mdash;<br>Some Clients/Studios/Friends</h3>
+                <p>Stance, Oakley, Kawasaki, Dusty Times, Thales, Fuse Interactice, NuContext</p>
+
             </div>
         </div>
-
-        <h2>&mdash;<br>Clients & Studios</h2>
-        <p>Stance, Oakley, Kawasaki, Dusty Times, Thales, Fuse Interactice, NuContext</p>
 
     </div>
 
