56afe9dc00ed36b70bf4ac3d9aa3ae5dc242bc31
Brett McMillin <brett@radsmth.com>
2020-05-01 13:07:36 -0700

    Pulled data in from Firebase

--- work/index.php
+++ work/index.php
@@ -11,67 +11,7 @@
 
     <?php include '../components/header.php';?>
 
-    <section class="display">
-
-        <div class="owl-carousel">
-            <img src="../lib/img/work/logos/rally.svg"
-                 data-project="Rally—Consumer Product"
-                 data-url="https://www.behance.net/gallery/92311931/Rally-Branding"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/mosaic.svg"
-                 data-project="Mosaic—Restaurant"
-                 data-url="https://www.behance.net/gallery/92308619/Mosaic-Restaurant-Branding"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/the-beer-house.svg"
-                 data-project="The Beer House—Bar"
-                 data-url="https://www.behance.net/gallery/92311293/The-Beer-House-Branding"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/dusty-times.svg"
-                 data-project="Dusty Times—Magazine"
-                 data-url="https://www.behance.net/gallery/92321687/Dusty-Times-Lifestyle-Journal"
-                 data-invert="true"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/mcm.svg"
-                 data-project="McM Sound—Agency"
-                 data-url="https://www.behance.net/gallery/92284455/McM-Sound-Branding"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/puparazzi.svg"
-                 data-project="Puparazzi—Small Business"
-                 data-url="https://www.behance.net/gallery/93039313/Puparazzi-Logo"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/boyd.svg"
-                 data-project="Boyd Photo—Small Business"
-                 data-url="https://www.behance.net/gallery/93040875/Boyd-Logo-Design"
-                 data-invert="true"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/fallon-40.svg"
-                 data-project="Fallon's 40th—Event"
-                 data-url="https://www.behance.net/gallery/93043327/Fallons-40th"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-            <img src="../lib/img/work/logos/dna.svg"
-                 data-project="DNA Media Lab—Company"
-                 data-url="https://www.behance.net/gallery/93041865/DNA-Media-Lab-Logo"
-                 data-invert="false"
-                 class="project-logo"
-                 >
-
-        </div>
-        
-    </section>
+    <section class="display" id="work-carousel"></section>
 
     <svg id="prev" class="arrow-left" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="60" height="32" viewBox="0 0 60 32" style="enable-background:new 0 0 60.7 32;" xml:space="preserve"><g transform="translate(0.5 0.5)"><line class="st0" x1="57.1" y1="15.5" x2="2.6" y2="15.5"/><polyline class="st1" points="11.6,24.5 2.6,15.5 11.6,6.5"/></g></svg>
     <svg id="next" class="arrow-right" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="60" height="32" viewBox="0 0 60 32" style="enable-background:new 0 0 60.7 32;" xml:space="preserve"><g transform="translate(0.5 0.5)"><line class="st0" x1="2.6" y1="15.5" x2="57.1" y2="15.5"/><polyline class="st1" points="48.1,6.5 57.1,15.5 48.1,24.5"/></g></svg>
