cbab98ff54b8d19d1e5596a484ceee89ba0838fe
Brett McMillin <brett@radsmth.com>
2020-05-02 08:57:45 -0700

    Built looping function to carousel

--- work/index.php
+++ work/index.php
@@ -13,10 +13,10 @@
 
     <section class="display" id="work-carousel"></section>
 
-    <!--
+    
     <svg id="prev" class="arrow-left" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="60" height="32" viewBox="0 0 60 32" style="enable-background:new 0 0 60.7 32;" xml:space="preserve"><g transform="translate(0.5 0.5)"><line class="st0" x1="57.1" y1="15.5" x2="2.6" y2="15.5"/><polyline class="st1" points="11.6,24.5 2.6,15.5 11.6,6.5"/></g></svg>
     <svg id="next" class="arrow-right" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="60" height="32" viewBox="0 0 60 32" style="enable-background:new 0 0 60.7 32;" xml:space="preserve"><g transform="translate(0.5 0.5)"><line class="st0" x1="2.6" y1="15.5" x2="57.1" y2="15.5"/><polyline class="st1" points="48.1,6.5 57.1,15.5 48.1,24.5"/></g></svg>
--->
+
 
     <div class="info">
         <span id="project-title-display"></span><br>
