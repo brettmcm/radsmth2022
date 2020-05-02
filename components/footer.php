20c47d847d2c239a13f303b756de233040ef3515
Brett McMillin <brett@radsmth.com>
2020-05-02 11:31:11 -0700

    Made site static

--- components/footer.php
+++
@@ -1,8 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-analytics.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-firestore.js"></script>
-
-
-
-<script src="../lib/js/app.js"></script>
